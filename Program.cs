77715f0062625d8d513940894dc4aa5585681f5e
dc morrison <nosirromd@gmail.com>
2022-03-26 21:38:38 +0000

    improve comments

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace SigningDemo
         {
             InitAliceKeys();
 
-            byte[] aliceData = Encoding.UTF8.GetBytes("Alice");
+            byte[] aliceData = Encoding.UTF8.GetBytes("Alice"); // her name
             byte[] aliceSignature = CreateSignature(aliceData, _aliceKeySignature);
             Console.WriteLine($"Alice created signature: {Convert.ToBase64String(aliceSignature)}");
 
@@ -33,16 +33,18 @@ namespace SigningDemo
         public void InitAliceKeys()
         {
             //create a key pair and export public key into a blob
-            _aliceKeySignature = CngKey.Create(CngAlgorithm.ECDsaP521);
-            _alicePubKeyBlob = _aliceKeySignature.Export(CngKeyBlobFormat.GenericPublicBlob);
+            _aliceKeySignature = CngKey.Create(CngAlgorithm.ECDsaP521); //key pair using the elliptic curve algorithm
+            _alicePubKeyBlob = _aliceKeySignature.Export(CngKeyBlobFormat.GenericPublicBlob); // serialise into a byte stream??
         }
 
         private byte[] CreateSignature(byte[] data, CngKey key)
         {
             // alice's name data element is signed using alice's private key
             byte[] signature;
-            using (var signingAlg = new ECDsaCng(key))
+            using (var signingAlg = new ECDsaCng(key)) // use the elliptic curve algorithm
             {
+                // compute a SHA512 hash of the byte array containing alice's name
+                // and sign the hash using EC 
                 signature = signingAlg.SignData(data, HashAlgorithmName.SHA512);
                 signingAlg.Clear();
             }
@@ -51,9 +53,10 @@ namespace SigningDemo
 
         private bool VerifySignature(byte[] data, byte[] signature, byte[] pubKey)
         {
-            // import key blob to obtain public key
+            // import key byte array to obtain public key object
             // create new crypto class instance and use it to
             // check that signature is indeed that of alice's
+            // return true is verified
             bool retValue = false;
             using (CngKey key = CngKey.Import(pubKey, CngKeyBlobFormat.GenericPublicBlob))
             using (var signingAlg = new ECDsaCng(key))
